d231d6f16f8a20c3b19a3e4bb36dcbb98a6474ec
sithu-go <sithugolang@gmail.com>
2024-09-16 20:32:33 +0630

    event

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 // ffff
-
+// dfdvfd
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
