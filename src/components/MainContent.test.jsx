e76ddd4f748372f59e7bd721177de39e8bfdd15f
sithu-go <sithugolang@gmail.com>
2024-09-16 20:33:21 +0630

    remove comment [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -3,8 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
-// ffff
-// dfdvfd
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
